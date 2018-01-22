8f5520cd94a5f980d13a13100b142c91063fbdc8
David Lee <david@Davids-MacBook-Air-2.local>
2018-01-22 10:36:53 -0800

    commented out ejs

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 
 app.use(express.static('public'));
 
+// app.set('view engine', 'ejs');
+
+// app.get('/posts', function(req, res) {
+//     res.render('views/about');
+// });
+
 if (require.main === module) {
 	app.listen(process.env.PORT || 8080, function () {
 		console.info(`App listening on ${this.address().port}`);
