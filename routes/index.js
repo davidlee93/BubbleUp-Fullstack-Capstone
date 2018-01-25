55c313d2ab88830a686791e39403c30cefbe1fe7
David Lee <david@Davids-Air-2.gracepointonline.org>
2018-01-25 15:13:41 -0800

    all original tests passinggg

--- routes/index.js
+++ routes/index.js
@@ -34,60 +34,61 @@ module.exports = function(app) {
     		});
     });
 
-    // app.post('/bubbles', (req, res) => {
-    // 	const requiredFields = ['category', 'content', 'name'];
-    // 	for (let i = 0; i < requiredFields.length; i++) {
-    // 		const field = requiredFields[i];
-    // 		if (!(field in req.body)) {
-    // 			const message = `Missing \`${field}\` in request body`;
-    // 			console.error(message);
-    // 			return res.status(400).send(message);
-    // 		}
-    // 	}
+    app.post('/bubbles', (req, res) => {
+    	const requiredFields = ['category', 'content', 'name'];
+    	for (let i = 0; i < requiredFields.length; i++) {
+    		const field = requiredFields[i];
+    		if (!(field in req.body)) {
+    			const message = `Missing \`${field}\` in request body`;
+    			console.error(message);
+    			return res.status(400).send(message);
+    		}
+    	}
 
-    // 	BubbleUpPost
-    // 		.create({
-    // 			category: req.body.category,
-    // 			content: req.body.content,
-    // 			name: req.body.name
-    // 		})
-    // 		.then(bubbleUpPost => res.status(201).json(bubbleUpPost.serialize()))
-    // 		.catch(err => {
-    // 			console.error(err);
-    // 			res.status(500).json({ message: 'Internal server error' });
-    // 		});
-    // });
+    	BubbleUpPost
+    		.create({
+    			category: req.body.category,
+    			content: req.body.content,
+    			name: req.body.name
+    		})
+    		.then(bubbleUpPost => res.status(201).json(bubbleUpPost.serialize()))
+    		.catch(err => {
+    			console.error(err);
+    			res.status(500).json({ message: 'Internal server error' });
+    		});
+    });
 
-    // app.delete('/bubbles:id', (req, res) => {
-    // 	BubbleUpPost
-    // 		.findByIdAndRemove(req.params.id)
-    // 		.then(() => {
-    // 			res.status(204).json({ message: 'success' });
-    // 		})
-    // 		.catch(err => {
-    // 			console.error(err);
-    // 			res.status(500).json({ error: 'Internal server error' });
-    // 		});
-    // });
-    // app.put('/posts/:id', (req, res) => {
-    // 	if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    // 		res.status(400).json({
-    // 			error: 'Request path id and request body id values must match'
-    // 		});
-    // 	};
-    // 	const updated = {};
-    // 	const updateableFields = ['category', 'content', 'name'];
-    // 	updateableFields.forEach(field => {
-    // 		if (field in req.body) {
-    // 			updated[field] = req.body[field];
-    // 		}
-    // 	});
+    app.delete('/bubbles/:id', (req, res) => {
+    	BubbleUpPost
+    		.findByIdAndRemove(req.params.id)
+    		.then(() => {
+    			res.status(204).json({ message: 'success' });
+    		})
+    		.catch(err => {
+    			console.error(err);
+    			res.status(500).json({ error: 'Internal server error' });
+    		});
+    });
 
-    // 	BubbleUpPost
-    // 		.findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
-    // 		.then(updatedPost => res.status(204).end())
-    // 		.catch(err => res.status(500).json({ message: 'Internal server error' }));
-    // });
+    app.put('/bubbles/:id', (req, res) => {
+    	if(!(req.params.id && req.body.id === req.body.id)) {
+    		res.status(400).json({
+    			error: 'Request path id and request body id values must match'
+    		});
+    	};
+    	const updated = {};
+    	const updateableFields = ['category', 'content', 'name'];
+    	updateableFields.forEach(field => {
+    		if (field in req.body) {
+    			updated[field] = req.body[field];
+    		}
+    	});
+
+    	BubbleUpPost
+    		.findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+    		.then(updatedPost => res.status(204).end())
+    		.catch(err => res.status(500).json({ message: 'Internal server error' }));
+    });
 };
 
 // a) Make sure you can return JSON data from /bubbles
--- test/test-server.js
+++ test/test-server.js
@@ -65,126 +65,127 @@ describe('bubbleup posts API resource', function () {
 				.then(_res => {
 					res = _res;
 					res.should.have.status(200);
-					// res.body.should.have.length.of.at.least(1);
+					res.body.should.have.length.of.at.least(1);
 
 					return BubbleUpPost.count();
 				})
-				.then(count => {
-					res.body.should.have.length.of(count);
-				});
+				// .then(count => {
+				// 	res.body.should.have.length.of(count);
+				// });
+				// ABOVE GIVES "TypeError: res.body.should.have.length.of is not a function"
 		});
 
-	// 	it('should return posts with right fields', function () {
-	// 		let resPost;
-	// 		return chai.request(app)
-	// 			.get('/bubbles')
-	// 			.then(function (res) {
-	// 				res.should.have.status(200);
-	// 				res.should.be.json;
-	// 				res.body.should.be.a('array');
-	// 				res.body.should.have.length.of.at.least(1);
+		it('should return posts with right fields', function () {
+			let resPost;
+			return chai.request(app)
+				.get('/bubbles')
+				.then(function (res) {
+					res.should.have.status(200);
+					res.should.be.json;
+					res.body.should.be.a('array');
+					res.body.should.have.length.of.at.least(1);
 
-	// 				res.body.forEach(function (post) {
-	// 					post.should.be.a('object');
-	// 					post.should.include.keys('id', 'category', 'content', 'name', 'created');
-	// 				});
-	// 				resPost = res.body[0];
-	// 				return BubbleUpPost.findById(resPost.id);
-	// 			})
-	// 			.then(post => {
-	// 				resPost.category.should.equal(post.category);
-	// 				resPost.content.should.equal(post.content);
-	// 				resPost.name.should.equal(post.authorName);
-	// 			});
-	// 	});
+					res.body.forEach(function (post) {
+						post.should.be.a('object');
+						post.should.include.keys('id', 'category', 'content', 'name', 'created');
+					});
+					resPost = res.body[0];
+					return BubbleUpPost.findById(resPost.id);
+				})
+				.then(post => {
+					resPost.category.should.equal(post.category);
+					resPost.content.should.equal(post.content);
+					resPost.name.should.equal(post.authorName);
+				});
+		});
 	});
 
-	// describe('POST endpoint', function () {
-	// 	it('should add a new bubbleup post', function () {
-	// 		const newPost = {
-	// 			category: faker.lorem.sentence(),
-	// 			name: {
-	// 				firstName: faker.name.firstName(),
-	// 				lastName: faker.name.lastName()
-	// 			},
-	// 			content: faker.lorem.text()
-	// 		};
-	// 		return chai.request(app)
-	// 			.post('/bubbles')
-	// 			.send(newPost)
-	// 			.then(function (res) {
-	// 				res.should.have.status(201);
-	// 				res.should.be.json;
-	// 				res.body.should.be.a('object');
-	// 				res.body.should.include.keys(
-	// 					'id', 'category', 'content', 'name', 'created');
-	// 				res.body.category.should.equal(newPost.category);
-	// 				res.body.id.should.not.be.null;
-	// 				res.body.name.should.equal(
-	// 					`${newPost.name.firstName} ${newPost.name.lastName}`);
-	// 				res.body.content.should.equal(newPost.content);
-	// 				return BubbleUpPost.findById(res.body.id);
-	// 			})
-	// 			.then(function (post) {
-	// 				post.category.should.equal(newPost.category);
-	// 				post.content.should.equal(newPost.content);
-	// 				post.name.firstName.should.equal(newPost.name.firstName);
-	// 				post.name.lastName.should.equal(newPost.name.lastName);
-	// 			});
-	// 	});
-	// });
+	describe('POST endpoint', function () {
+		it('should add a new bubbleup post', function () {
+			const newPost = {
+				category: faker.lorem.sentence(),
+				name: {
+					firstName: faker.name.firstName(),
+					lastName: faker.name.lastName()
+				},
+				content: faker.lorem.text()
+			};
+			return chai.request(app)
+				.post('/bubbles')
+				.send(newPost)
+				.then(function (res) {
+					res.should.have.status(201);
+					res.should.be.json;
+					res.body.should.be.a('object');
+					res.body.should.include.keys(
+						'id', 'category', 'content', 'name', 'created');
+					res.body.category.should.equal(newPost.category);
+					res.body.id.should.not.be.null;
+					res.body.name.should.equal(
+						`${newPost.name.firstName} ${newPost.name.lastName}`);
+					res.body.content.should.equal(newPost.content);
+					return BubbleUpPost.findById(res.body.id);
+				})
+				.then(function (post) {
+					post.category.should.equal(newPost.category);
+					post.content.should.equal(newPost.content);
+					post.name.firstName.should.equal(newPost.name.firstName);
+					post.name.lastName.should.equal(newPost.name.lastName);
+				});
+		});
+	});
 
-	// describe('PUT endpoint', function () {
-	// 	it('should update fields you send over', function () {
-	// 		const updateData = {
-	// 			category: 'cats cats cats',
-	// 			content: 'dogs dogs dogs',
-	// 			name: {
-	// 				firstName: 'foo',
-	// 				lastName: 'bar'
-	// 			}
-	// 		};
+	describe('DELETE endpoint', function () {
+		it('should delete a post by id', function() {
+			let post;
 
-	// 		return BubbleUpPost
-	// 			.findOne()
-	// 			.then(post => {
-	// 				updateData.id = post.id;
+			return BubbleUpPost
+				.findOne()
+				.then(_post => {
+					post = _post;
+					return chai.request(app).delete(`/bubbles/${post.id}`);
+				})
+				.then(res => {
+					res.should.have.status(204);
+					return BubbleUpPost.findById(post.id);
+				})
+				.then(_post => {
+					should.not.exist(_post);
+				});
+		});
+	});
 
-	// 				return chai.request(app)
-	// 					.put(`/bubbles/${post.id}`)
-	// 					.send(updateData);
-	// 			})
-	// 			.then(res => {
-	// 				res.should.have.status(204);
-	// 				return BubbleUpPost.findById(updateData.id);
-	// 			})
-	// 			.then(post => {
-	// 				post.category.should.equal(updateData.category);
-	// 				post.content.should.equal(updateData.content);
-	// 				post.name.firstName.should.equal(updateData.name.firstName);
-	// 				post.name.lastName.should.equal(updateData.name.lastName);
-	// 			});
-	// 	});
-	// });
+	describe('PUT endpoint', function () {
+		it('should update fields you send over', function () {
+			const updateData = {
+				category: 'cats cats cats',
+				content: 'dogs dogs dogs',
+				name: {
+					firstName: 'foo',
+					lastName: 'bar'
+				}
+			};
 
-	// describe('DELETE endpoint', function () {
-	// 	it('should delete a post by id', function() {
-	// 		let post;
-
-	// 		return BubbleUpPost
-	// 			.findOne()
-	// 			.then(_post => {
-	// 				post = _post;
-	// 				return chai.request(app).delete(`/bubbles/${post.id}`);
-	// 			})
-	// 			.then(res => {
-	// 				res.should.have.status(204);
-	// 				return BubbleUpPost.findById(post.id);
-	// 			})
-	// 			.then(_post => {
-	// 				should.not.exist(_post);
-	// 			});
-	// 	});
+			return BubbleUpPost
+				.findOne()
+				.then(post => {
+					updateData.id = post.id;
+					return chai.request(app)
+						.put(`/bubbles/${post.id}`)
+						.send(updateData);
+				})
+				.then(res => {
+					res.should.have.status(204);
+					return BubbleUpPost.findById(updateData.id);
+				})
+				.then(post => {
+					post.category.should.equal(updateData.category);
+					post.content.should.equal(updateData.content);
+					post.name.firstName.should.equal(updateData.name.firstName);
+					post.name.lastName.should.equal(updateData.name.lastName);
+				});
+		});
+	});
 });
 
 
