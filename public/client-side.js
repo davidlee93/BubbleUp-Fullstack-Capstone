b3f38610c03a8b3e4e22d69c8a42bb34f3ff170c
david <davidlee93@gmail.com>
2018-01-31 10:17:27 -0800

    Revert "Revert "test4 got post updated when I was working on Edit Me bubble and the database info does not seem to match with each other which shows different key values when comparing seed date with new bubbles""
    This reverts commit dbc3e380fc3b7b2c9506286ee9e3f77653d9b357.

--- public/client-side.js
+++ public/client-side.js
@@ -1,5 +1,5 @@
-var MOCK_POST_UPDATES = {
-    "postUpdates": [
+var MOCK_POSTS = {
+    "posts": [
     {
         "name": {
             "firstName": "Test1",
@@ -9,7 +9,7 @@ var MOCK_POST_UPDATES = {
         "content": "https://youtuve.com/video",
         "contentType": "Video",
         "created": "1470012976609"
-    }
+    },
     {
         "name": {
             "firstName": "Test2",
@@ -19,7 +19,7 @@ var MOCK_POST_UPDATES = {
         "content": "https://youtuve.com/video/motivational",
         "contentType": "Video",
         "created": "1470012976610"
-    }
+    },
     {
         "name": {
             "firstName": "Test3",
@@ -29,7 +29,7 @@ var MOCK_POST_UPDATES = {
         "content": "Man does not live by bread alone",
         "contentType": "quote",
         "created": "1470012976611"
-    }
+    },
     {
         "name": {
             "firstName": "Test4",
@@ -39,7 +39,7 @@ var MOCK_POST_UPDATES = {
         "content": "Got to do ____ on a daily basis",
         "contentType": "text",
         "created": "1470012976612"
-    }
+    },
     {
         "name": {
             "firstName": "Test5",
@@ -52,66 +52,201 @@ var MOCK_POST_UPDATES = {
     }]
 };
 
-function getRecentPostUpdates(callbackFn) {
-    // $('.bubbles-onclick').onclick(event => {
-    //     callbackFn(MOCK_POST_UPDATES);
-    // });
-
-    setTimeout(function(){ callbackFn(MOCK_POST_UPDATES)}, 1000);
+// state object
+state = {
+    currentPost: {
+        category: '',
+        content: '',
+        contentType: ''
+    },
+    posts: []
 }
 
-// this function stays the same when we connect
-// to real API later
-function displayPostUpdates(data) {
-    for (index in data.postUpdates) {
-       $('body').append(
+// Call API functions
+function getPosts() {
+    // $.ajax({
+    //     method: "GET",
+    //     url: "/bubbles",
+    //     headers: { 'Content-Type': 'application/json'}
+    // }
+    fetch('/bubbles', {
+        method: "GET",
+        headers: { 'Content-Type': 'application/json'}
+    })
+    .then(response => response.json())
+    .then(bubbles => displayPosts(bubbles))
+    .catch(error => console.log(error));
+}
+
+function createBubble() {
+
+    const post = {
+        "name": {
+            "firstName": "Edit",
+            "lastName": "Me"
+        },
+        "category": "Needs Edit",
+        "content": "Bubble needs editing",
+        "contentType": "text",
+        "created": "just now"
+    }
+    const options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(post)
+    }
+    fetch(`/bubbles`, options)
+    .then(response => response.json())
+    .then(bubble => displayNewPost(bubble))
+    .catch(error => console.log(error));
+}
+
+function displayNewPost(data) {
+   $('.bubbles').append(
+    `<div class="post col-md-4">
+        <h3>${data.name}</h3>
+        <h3># ${data.category}</h3>
+        <p>${data.content}</p>
+        <h5>${data.created}</h5>
+        <a href="#" class="deleteBubble" data-id=${data.id}>Delete bubble</a>
+        <div id="panel">
+            <a href="#" class="editBubble">Edit bubble</a>
+            <div class="dialog" id="myform" tabindex="-1" data-backdrop="false">
+                <form>
+                    <label id="Edit Page">
+                    </label>
+                    <input type="text" name="category" id="name">
+                    <input type="text" name="content" id="content">
+                    <input type="text" name="contentType" id="contentType">
+                    <div align="center">
+                        <input type="button" value="Edit" id="btnOK" data-id=${data.id}>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>`
+    );
+}
+// <input type="text" name="category" id="category" ex.value="Sports" />
+// <button id="create-post-button" onclick="grabPost()" /> 
+ 
+//  function grabPost () {
+//     const post = {
+//            category: $("#category").val(),
+//            content: $("#content").val(),
+//            contentType: $("contentType").val()
+//     }
+//     // set state currentPost to post value from create form
+//     state.currentPost = post
+//  }
+ 
+// $("#create-post-button").click({
+//     createPost(state.currentPost)
+// })
+
+function updatePost(id) {
+    
+    const post = {
+        "category": $("#category").val(),
+        "content": $("#content").val(),
+        "contentType": $("#contentType").val()
+    }
+    state.currentPost = post;
+
+    const options = {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(state.currentPost)
+    }
+    fetch(`/bubbles/${id}`, options)
+    .then(response => response.json())
+    .then(bubble => displayNewPost(bubble))
+    .catch(error => console.log(error));
+}
+
+function deleteBubble(bubbleId) {
+    console.log(bubbleId)
+    fetch(`/bubbles/${bubbleId}`, {
+        method: "DELETE",
+        headers: { 'Content-Type': 'application/json'}
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.log(error));
+}
+
+// display data from API
+function displayPosts(data) {
+    for (index in data) {
+       $('.bubbles').append(
         `<div class="post col-md-4">
-            <p>${data.postUpdates[index].friendName}</p>
-            <p>${data.postUpdates[index].text}</p>
-            <p>${data.postUpdates[index].publishedAt}</p>
+            <h3>${data[index].name}</h3>
+            <h3># ${data[index].category}</h3>
+            <p>${data[index].content}</p>
+            <h5>${data[index].created}</h5>
+            <a href="#" class="deleteBubble" data-id=${data[index].id}>Delete bubble</a>
+            <div id="panel">
+                <a href="#" class="editBubble">Edit bubble</a>
+                <!-- Dialog Box-->
+                <div class="dialog" id="myform" tabindex="-1" data-backdrop="false">
+                    <form>
+                        <label id="editModal">Edit Page
+                        </label>
+                        <br>
+                        <label for="category">Category:</label>
+                        <input type="text" name="category" id="category">
+                        <label for="content">Content:</label>
+                        <input type="text" name="content" id="content">
+                        <label for="contentType">Content Type:</label>
+                        <input type="text" name="contentType" id="contentType">
+                        <div align="center">
+                            <input type="button" value="Edit" id="btnOK" data-id=${data[index].id}>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>`
         );
     }
 }
 
-// this function can stay the same even when we
-// are connecting to real API
-function getAndDisplayPostUpdates() {
-    getRecentPostUpdates(displayPostUpdates);
-}
+// Event handlers to call API functions
+getPosts()
 
-$(function() {
-    getAndDisplayPostUpdates();
-})
+$(document).on('click', '.editBubble', function() {
+    $("#myform input[type=text]").val('');
+    $("#myform").show(500);
+});
 
+$(document).on('click', '#btnOK', function() {
+    const bubbleId = $(this).data('id');
+    updatePost(bubbleId);
+    $("#myform").hide(400);
+});
 
+// $(function() {
+//     $(".edit-button").click(function() {
+//         $("#myform #valueFromMyButton").text($(this).val().trim());
+//         $("#myform input[type=text]").val('');
+//         $("#valueFromMyModal").val('');
+//         $("#myform").show(500);
+//     });
+//     $("#btnOK").click(function() {
+//         $("#valueFromMyModal").val($("#myform input[type=text]").val().trim());
+//         $("#myform").hide(400);
+//     });
+// });
 
+$(document).on('click', '.deleteBubble', function(){
+    event.preventDefault();
+    const bubbleId = $(this).data('id');
+    deleteBubble(bubbleId);
+    $(this).parent().remove();
 
+});
 
-// function displayData(data) {
-//     //console.log(data)
-//     const posts = data.map(post => {
-//         return `<div>${post.name}</div>`
-//     })
-//     let div = document.createElement("div");
-//     div.innerHTML = posts;
-//     document.getElementById('posts').appendChild(div)
-// }
-
-// function getPosts() {
-//     fetch('/posts')
-// .then(response => response.json())
-// .then(data => displayData(data))
-// .catch(error => console.log(error))
-// }
-
-// function deletePost(id){
-//     fetch(`/posts/${id}`)
-//     .then()
-// }
-
-// getPosts()
-
-
-
+$(document).on('click', '.createBubble', function(){
+    event.preventDefault();
+    createBubble();
+});
 
