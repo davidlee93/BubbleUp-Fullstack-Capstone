72d65e1ac599e0a70ba3f14cf30ba5e398333e4b
David Lee <davidlee@Noahs-MacBook-Pro-2.local>
2018-04-12 09:52:42 -0700

    reverted back to 127

--- routes/config.js
+++ routes/config.js
@@ -1,5 +1,5 @@
 'use strict'
 
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost:27017/bubbleup';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/bubbleup-test';
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://127.0.0.1:27017/bubbleup';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://127.0.0.1:27017/bubbleup-test';
 exports.PORT = process.env.PORT || 8080;
